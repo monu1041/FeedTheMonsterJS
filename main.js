0d64ef5f591810cc7bbdbdc9b2389e2717f1b588
Vinay <62736791+vinay961861@users.noreply.github.com>
2022-08-01 12:38:47 +0530

    Initial changes

--- main.js
+++ main.js
@@ -1,96 +1,127 @@
 let canvas = document.getElementById("canvas");
-// let canvasFench = document.getElementById("canvas-fench");
 let scene = document.getElementsByClassName("game-scene");
 
-// canvas.style.display = "none";
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
+scene.innerHTML =
+  "<canvas style='z-index:1;' id='timer' width='" +
+  canvas.width +
+  "' height='" +
+  100 +
+  "'>";
+let ctx = canvas.getContext("2d");
+console.log("heighe", canvas.height);
 
-// class LessonCOntroller() {
-//     constructor(){
-//         this.scene = new StartNew();
-//     }
+var bgImg = new Image();
+bgImg.src = "./assets/images/bg_v01.jpg";
+bgImg.onload = function (e) {
+  createBackground();
+};
 
-//     render () {
+var hillImg = new Image();
+hillImg.src = "./assets/images/hill_v01.png";
+hillImg.onload = function (e) {
+  createBackground();
+};
+var timer_empty = new Image();
+timer_empty.src = "./assets/images/timer_empty.png";
+timer_empty.onload = function (e) {
+  createBackground();
+};
 
-//     }
-// }
+var pillerImg = new Image();
+pillerImg.src = "./assets/images/Totem_v02_v01.png";
+pillerImg.onload = function (e) {
+  createBackground();
+};
 
-// let lenss = new LessonCOntroller()
-// lenss.render()
+var grassImg = new Image();
+grassImg.src = "./assets/images/FG_a_v01.png";
+grassImg.onload = function (e) {
+  createBackground();
+};
+var rotating_clock = new Image();
+rotating_clock.src = "./assets/images/timer.png";
+rotating_clock.onload = function (e) {
+  createBackground();
+};
+var timer_full = new Image();
+timer_full.src = "./assets/images/timer_full.png";
+timer_full.onload = function (e) {
+  createBackground();
+};
 
-// console.log(scene)
+var fenchImg = new Image();
+fenchImg.src = "./assets/images/fence_v01.png";
+fenchImg.onload = function (e) {
+  createBackground();
+};
 
-// var viewWidth = scene.width;
-// var viewHeigth = scene.height;
+function createBackground() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    pillerImg,
+    canvas.width * 0.6,
+    canvas.height / 6,
+    canvas.width,
+    canvas.height / 2
+  );
+  ctx.drawImage(
+    fenchImg,
+    -canvas.width * 0.4,
+    canvas.height / 3,
+    canvas.width,
+    canvas.height / 3
+  );
+  ctx.drawImage(
+    hillImg,
+    -canvas.width * 0.25,
+    canvas.height / 2,
+    canvas.width * 1.5,
+    canvas.height / 2
+  );
+  ctx.drawImage(
+    grassImg,
+    -canvas.width * 0.25,
+    canvas.height / 2 + (canvas.height / 2) * 0.1,
+    canvas.width * 1.5,
+    canvas.height / 2
+  );
+  ctx.drawImage(
+    timer_empty,
+    0,
+    canvas.height * 0.1,
+    canvas.width,
+    canvas.height * 0.05
+  );
+  ctx.drawImage(
+    rotating_clock,
+    5,
+    canvas.height * 0.09,
+    canvas.width * 0.12,
+    canvas.height * 0.06
+  );
+  ctx.drawImage(
+    timer_full,
+    canvas.width * 0.12,
+    canvas.height * 0.1,
+    canvas.width - canvas.width * 0.12,
+    canvas.height * 0.05
+  );
+}
+function startTheTimer() {
 
-// console.log(viewHeigth);
-// console.log(viewWidth)
-
-
-let ctx = canvas.getContext('2d');
-// let ctxFench = canvas.getContext('2d')
-
-// canvas.width = window.innerWidth/2;
-// canvas.height = window.innerHeight;
-
-// window.addEventListener("resize", function(){
-//     canvas.width = window.innerWidth/2;
-// });
-// canvas.width = 400;
-canvas.height = window.innerHeight
-
-
-var bgimg = new Image();
-bgimg.src = "./assets/images/bg_v01.jpg";
-bgimg.onload = function(e){
-    drawBackdown();
+  setInterval(() => {
+    ctx.drawImage(
+      timer_full,
+      canvas.width * 0.12,
+      canvas.height * 0.1,
+      canvas.width * 0.1,
+      canvas.height * 0.05
+    );
+  }, 1000);
 }
 
-var hillimg = new Image();
-hillimg.src = "./assets/images/hill_v01.png";
-hillimg.onload = function(e){
-    drawBackdown();
-}
-
-var pillerimg = new Image();
-pillerimg.src = "./assets/images/Totem_v02_v01.png";
-pillerimg.onload = function(e){
-    drawBackdown();
-}
-
-var grassimg = new Image();
-grassimg.src = "./assets/images/FG_a_v01.png";
-grassimg.onload = function(e){
-    drawBackdown();
-}
-
-var fenchimg = new Image();
-fenchimg.src = "./assets/images/fence_v01.png";
-fenchimg.onload = function(e){
-    ctx.save(); // save current state
-    ctx.translate(-220, 25)
-    ctx.rotate(-120); // rotate
-    drawBackdown1();
-    // ctx.restore(); 
-}
-
-
-function drawBackdown() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(bgimg, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(pillerimg, canvas.width * .6, canvas.height/6, canvas.width, canvas.height/2);
-    ctx.drawImage(hillimg, -canvas.width * .25, canvas.height/2, canvas.width * 1.5, canvas.height/2);
-    ctx.drawImage(grassimg, -canvas.width * .25, canvas.height/2 + canvas.height/2 * 0.10, canvas.width * 1.5, canvas.height/2);
-    
-}
-
-function drawBackdown1() {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // ctx.drawImage(bgimg, 0, 0, canvas.width, canvas.height);
-    // ctx.drawImage(pillerimg, canvas.width * .6, canvas.height/6, canvas.width, canvas.height/2);
-    ctx.drawImage(fenchimg, -canvas.width * .26, canvas.height/2, canvas.width/1.25, canvas.height/5.25);
-    // ctx.drawImage(hillimg, -canvas.width * .25, canvas.height/2, canvas.width * 1.5, canvas.height/2);
-    // ctx.drawImage(grassimg, -canvas.width * .25, canvas.height/2 + canvas.height/2 * 0.10, canvas.width * 1.5, canvas.height/2);
-    
-}
-
+startTheTimer();
