1e27668bd15bd7076c1bcbed0c40bb43b2353e76
Vinay <62736791+vinay961861@users.noreply.github.com>
2022-08-02 11:26:37 +0530

    removed unused things

--- main.js
+++ main.js
@@ -46,23 +46,6 @@ rotating_clock.src = "./assets/images/timer.png";
 rotating_clock.onload = function (e) {
   createBackground();
 };
-var score_card = new Image();
-score_card.src = "./assets/images/score_v01.png";
-score_card.onload = function (e) {
-  createBackground();
-};
-var pause_button = new Image();
-pause_button.src = "./assets/images/pause_v01.png";
-pause_button.onload = function (e) {
-  createBackground();
-  pause_button.addEventListener(
-    "click",
-    function (evt) {
-      console.log(pause_button.y);
-    },
-    false
-  );
-};
 var timer_full = new Image();
 timer_full.src = "./assets/images/timer_full.png";
 timer_full.onload = function (e) {
@@ -76,7 +59,7 @@ fenchImg.onload = function (e) {
 };
 
 function createBackground() {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(
     pillerImg,
@@ -120,28 +103,6 @@ function createBackground() {
     canvas.width * 0.12,
     canvas.height * 0.06
   );
-  ctx.drawImage(
-    timer_full,
-    canvas.width * 0.12,
-    canvas.height * 0.1,
-    canvas.width - canvas.width * 0.12,
-    canvas.height * 0.05
-  );
-  ctx.drawImage(
-    score_card,
-    canvas.width * 0.01,
-    canvas.height * 0.01,
-    canvas.width * 0.35,
-    canvas.height * 0.07
-  );
-  ctx.drawImage(
-    pause_button,
-    canvas.width * 0.75,
-    canvas.height * 0.005,
-    canvas.width * 0.2,
-    canvas.height * 0.08
-  );
-}
 //   ctx.drawImage(
 //     timer_full,
 //     canvas.width * 0.11,
@@ -149,6 +110,7 @@ function createBackground() {
 //     canvas.width - 150,
 //     canvas.height * 0.051
 //   );
+}
 
 function createBackground1() {
     timerCtx.drawImage(
