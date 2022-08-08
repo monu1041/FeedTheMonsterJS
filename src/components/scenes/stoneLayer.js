f2a0dcb2ea36097cbb0538303e9fda001a5678bf
Vinay <62736791+vinay961861@users.noreply.github.com>
2022-08-08 17:35:58 +0530

    stone Dragging

--- src/components/scenes/stoneLayer.js
+++ src/components/scenes/stoneLayer.js
@@ -13,7 +13,7 @@ var gs = {
 gs.puzzle = {
   puzzlenum: 0,
   target: "e",
-  stones: ["e", "m"],
+  stones: ["e", "d"],
 };
 gs.stones = [];
 var pickedStone;
@@ -29,14 +29,26 @@ export default class StoneLayer {
     this.context = document.getElementById(this.id).getContext("2d");
     document.getElementById(this.id).style.zIndex = 5;
     document.getElementById(this.id).addEventListener(
-      "click",
+      "mousedown",
       function (event) {
         var rect = document.getElementById(this.id).getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
+        for (let s of gs.stones) {
+          if (Math.sqrt((x - s.x) * (x - s.x) + (y - s.y) * (y - s.y)) <= 32) {
+            pickedStone = s;
+          }
+        }
       },
       false
     );
+    document
+      .getElementById(this.id)
+      .addEventListener("mouseup", function (event) {
+        pickedStone.x =pickedStone.origx
+        pickedStone.y =pickedStone.origy
+        pickedStone = null
+      }, false);
     document.getElementById(this.id).addEventListener(
       "mousemove",
       function (event) {
@@ -66,12 +78,12 @@ export default class StoneLayer {
   }
   createStones() {
     var poss = stonepos[0];
-    gs.puzzle.stones = ["c", "m"];
+    gs.puzzle.stones = ["c", "d"];
     var i = 0;
     for (let s of gs.puzzle.stones) {
       console.log(poss[i][0]);
       var ns = new Stone(s, poss[i][0], poss[i][1]);
-      pickedStone = ns;
+      // pickedStone = ns;
       gs.stones.push(ns);
       i += 1;
     }
