a5aa0fbe02500ccfd6d0d24a327fb46b7f9f82c0
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2022-08-19 10:14:53 +0530

    AdditionalTouchEvents

--- src/scenes/level-selection-scene.js
+++ src/scenes/level-selection-scene.js
@@ -39,13 +39,13 @@ export class AllLevelScreen {
       [
         [this.canvas.width/5-offsetCoordinateValue,this.canvas.height/6-offsetCoordinateValue],
         [this.canvas.width/2-offsetCoordinateValue,this.canvas.height/6-offsetCoordinateValue],
-        [this.canvas.width/3.5+this.canvas.width/2-offsetCoordinateValue,this.canvas.height/6-offsetCoordinateValue],
+        [this.canvas.width/3+this.canvas.width/2.1-offsetCoordinateValue,this.canvas.height/6-offsetCoordinateValue],
         [this.canvas.width/5-offsetCoordinateValue,this.canvas.height/2.8-offsetCoordinateValue],
         [this.canvas.width/2-offsetCoordinateValue,this.canvas.height/2.8-offsetCoordinateValue],
         [this.canvas.width/3+this.canvas.width/2.1-offsetCoordinateValue,this.canvas.height/2.8-offsetCoordinateValue],
         [this.canvas.width/20+this.canvas.width/7-offsetCoordinateValue,this.canvas.height/1.8-offsetCoordinateValue],
         [this.canvas.width/2-offsetCoordinateValue,this.canvas.height/1.8-offsetCoordinateValue],
-        [this.canvas.width/3.1+this.canvas.width/2.1-offsetCoordinateValue,this.canvas.height/1.8-offsetCoordinateValue],
+        [this.canvas.width/3+this.canvas.width/2.1-offsetCoordinateValue,this.canvas.height/1.8-offsetCoordinateValue],
         [this.canvas.width/5-offsetCoordinateValue,this.canvas.height/1.3-offsetCoordinateValue],
 
       ],
@@ -53,55 +53,34 @@ export class AllLevelScreen {
     
    
         
-    // document.getElementById(this.id).addEventListener(
-    //   "mousemove",
-    //   function (event) {
-    //     var rect = document.getElementById(this.id).getBoundingClientRect();
-    //     const x = event.clientX - rect.left;
-    //     const y = event.clientY - rect.top;
-    //     if (pickedStone) {
-    //       pickedStone.x = x;
-    //       pickedStone.y = y;
-    //     }
-    //   },
-    //   false
-    // );
+   document.getElementById(this.id).addEventListener("touchend",function(event){
+    event.preventDefault();
+    
+    var touch = event.changedTouches[0];
+        var mouseEvent = new MouseEvent("mouseup", {
+          clientX: touch.clientX,
+          clientY: touch.clientY,
+        });
+        const x = touch.clientX ;
+        const y = touch.clientY; 
+        var imageSize = canvas.height/5;
+        for (let s of gs.levels) {
+            if (Math.sqrt((s.x+(imageSize/8))-x/1.3) <15 && Math.sqrt((s.y+(imageSize/8))-y)<12 ) {
+                console.log(s.index);
+              }
+        }
+        document.getElementById(this.id).dispatchEvent(mouseEvent);
 
-    // document.getElementById(this.id).addEventListener(
-    //   "touchstart",
-    //   function (e) {
-    //     var touch = e.touches[0];
-    //     var mouseEvent = new MouseEvent("mousedown", {
-    //       clientX: touch.clientX,
-    //       clientY: touch.clientY,
-    //     });
-    //     document.getElementById(this.id).dispatchEvent(mouseEvent);
-    //   },
-    //   false
-    // );
-
-    // document.getElementById(this.id).addEventListener(
-    //   "touchmove",
-    //   function (e) {
-    //     var touch = e.touches[0];
-    //     var mouseEvent = new MouseEvent("mousemove", {
-    //       clientX: touch.clientX,
-    //       clientY: touch.clientY,
-    //     });
-    //     document.getElementById(this.id).dispatchEvent(mouseEvent);
-    //   },
-    //   false
-    // );
-
+   },false)
+  
     document.getElementById(this.id).addEventListener(
       "mousedown",
       function (event) {
-        // this.scene =LevelStartScene(this.game);
         
+        event.preventDefault();
         var rect = document.getElementById(self.id).getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;    
-        // console.log(r);   
         for (let s of gs.levels) {
           // console.log("hiiiiiiii",Math.sqrt((x - s.x) * (x - s.x) + (y - s.y) * (y - s.y)));
           // console.log(s.x, s.y, x,y);
@@ -158,131 +137,3 @@ export class AllLevelScreen {
     // this.pauseButton.draw();
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import {gameData } from "./data.js"
-// import { levelButton } from "./levelButton.js";
-// import { StoneConfig } from "./src/common/stones-config.js";
-// import {LevelStartScene} from './src/scenes/level-start-scene.js';
-// import { CanvasStack } from "./src/utility/canvas-stack.js";
-// const gamescene = document.querySelector(".game-scene"); 
-// const canvas = document.getElementById("canvas");
-// const newCanvas= document.getElementById("new-canvas");
-// // newCanvas.style.height =window.innerHeight;
-// // newCanvas.style.width=window.screen.width > 420 ? 420 : window.innerWidth;
-// var mapIcon= new Image();
-// mapIcon.src= "./assets/images/mapIcon.png";
-
-// var levelData={
-
-// }
-
-// levelData.data={
-//   levelNumber:[1,2,3,4,5,6],
-// };
-// levelData.levelNumber=[];
-// var offsetCoordinateValue=32;
-// export class AllLevelScreen{
-// constructor(game){
-// this.game= game;
-// this.game.height= window.innerHeight;
-// this.game.width = window.screen.width > 420 ? 420 : window.innerWidth;
-// this.canvasStack = new CanvasStack("canvas");
-// this.height=window.innerHeight;
-// this.width=window.screen.width > 420 ? 420 : window.innerWidth;
-// this.createButton();
-// }
-// createButton(){
-
-//   var self = this;
-//   this.id = this.canvasStack.createLayer(this.height,this.width);
-//   this.context = document.getElementById(this.id).getContext("2d");
-//   // for(let i=0;i<5;i++){
-//   // this.context.drawImage(mapIcon,10,10,100,100);
-//   // this.context.fillText(i+1, 55, 50);
-//   // this.context.fillText(gameData.Levels[i].LevelMeta.LevelType, 55, 50);
-//   // }
-//   document.getElementById(this.id).style.zIndex = 10;
-  
-//   this.stonepos=[
-//     [
-//       [this.canvas.width/7-offsetCoordinateValue,this.canvas.height/1.9-offsetCoordinateValue],
-//       [this.canvas.width/2-offsetCoordinateValue,this.canvas.height/1.15-offsetCoordinateValue],
-//       [this.canvas.width/3.5+this.canvas.width/2-offsetCoordinateValue,this.canvas.height/1.2-offsetCoordinateValue],
-//       [this.canvas.width/4-offsetCoordinateValue,this.canvas.height/1.28-offsetCoordinateValue],
-//       [this.canvas.width/7-offsetCoordinateValue,this.canvas.height/1.5-offsetCoordinateValue],
-//       [this.canvas.width/2.3+this.canvas.width/2.1-offsetCoordinateValue,this.canvas.height/1.9-offsetCoordinateValue],
-//       [this.canvas.width/2.3+this.canvas.width/2.1-offsetCoordinateValue,this.canvas.height/1.42-offsetCoordinateValue],
-//       [this.canvas.width/6-offsetCoordinateValue,this.canvas.height/1.15-offsetCoordinateValue],
-
-//     ],
-//   ];
-//   this.createStones(this.stonepos);
-//   }
-
-// createStones(stonepos){
-//  var poss = stonepos[0];
-//  let i=0;
-//  for(let s of levelData.data.levelNumber){
-//   console.log("hello");
-//   var ns = new StoneConfig(s,poss[i][0],poss[i][1]);
-//   levelData.levelNumber.push(ns);
-//   i+=1;
-//  }
-//  this.draw();
-// }
-
-// draw(){
-//   for (let s of gs.stones) {
-//     this.drawstone(s,canvas);
-//   }
-// }
-// drawstone(s,canvas) {
-//   var imageCenterOffsetX = canvas.height/60-canvas.height/30;
-//   var imageCenterOffsetY = -canvas.height/60-canvas.height/44;
-//   var imageSize = canvas.height/18;
-//   var textFontSize = canvas.height/30
-
-//   this.context.drawImage(s.img, s.x+imageCenterOffsetX, s.y+imageCenterOffsetY, imageSize, imageSize);
-//   this.context.fillStyle = "white";
-//   this.context.font = textFontSize+"px Arial";
-//   this.context.fillText(s.text, s.x , s.y);
-// }
-// }
