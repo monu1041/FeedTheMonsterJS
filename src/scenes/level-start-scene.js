51a960f4a7a5b19b3e62bd9b8d14d58c1c679fde
Rakshith <62013445+rakshith-arch@users.noreply.github.com>
2022-08-22 06:48:53 +0530

    Rendering of data using Json data

--- src/scenes/level-start-scene.js
+++ src/scenes/level-start-scene.js
@@ -19,6 +19,7 @@ var images = {
 };
 var self;
 var length = 0;
+var currentPuzzleArray=0;
 export class LevelStartScene {
   constructor(game, puzzleData) {
     this.game = game;
@@ -33,32 +34,41 @@ export class LevelStartScene {
       game,
       game.width,
       game.height,
-      puzzleData[0],
+      puzzleData[currentPuzzleArray],
       this.redrawOfStones
     );
     this.puzzleData = puzzleData;
   }
-
+  
   redrawOfStones(status) {
-    self.stones.deleteCanvas();
+    // self.stones.deleteCanvas();
     // if (length >= 3) {
     //   new LevelEndScene(self.game,2);
     // }
-
+  //  self.stones.a=42;
     if (status) {
       self.monster.changeToEatAnimation();
+      currentPuzzleArray+=1;
     } else {
       self.monster.changeToSpitAnimation();
+      currentPuzzleArray+=1;
+
     }
     setTimeout(() => {
-      self.stones = new StonesLayer(
-        self.game,
-        self.game.width,
-        self.game.height,
-        self.puzzleData[1],
-        self.redrawOfStones
-      );
-    }, 2000);
+      // self.stones.setNewPuzzle(self.puzzleData[currentPuzzleArray])
+      // self.stones.draw();
+      
+      // self.stones = new StonesLayer(
+      //   self.game,
+      //   self.game.width,
+      //   self.game.height,
+      //   self.puzzleData[1],
+      //   self.redrawOfStones
+      // );
+      self.stones.setNewPuzzle(self.puzzleData[currentPuzzleArray])
+      self.stones.canvas.scene.levelIndicators.setIndicators(currentPuzzleArray);
+    },3000);
+    
   }
   createCanvas() {
     this.id = this.canvasStack.createLayer(this.height, this.width);
@@ -145,7 +155,7 @@ export class LevelStartScene {
     this.context.fillStyle = "black";
     this.context.font = 30 + "px Arial";
     this.context.fillText(
-      this.puzzleData[0].targetStones[0],
+      this.puzzleData[currentPuzzleArray].targetStones[0],
       this.width / 2.1,
       this.height * 0.26
     );
