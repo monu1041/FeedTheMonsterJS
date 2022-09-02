6ecb958994814b54ee3bc59c6c93790698a8b143
Vinay <62736791+vinay961861@users.noreply.github.com>
2022-09-02 12:54:03 +0530

    Update firebase_integration.js

--- src/firebase/firebase_integration.js
+++ src/firebase/firebase_integration.js
@@ -8,13 +8,11 @@ export class FirebaseIntegration {
       /*webpackIgnore: true*/ "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js"
     ).then((init) => {
       this.app = init.initializeApp(firebaseConfig);
-      console.log('!!!!!!',this.app)
     });
     import(
       /*webpackIgnore: true*/ "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js"
     ).then((analytics) => {
       this.analytics = analytics.getAnalytics(this.app);
-      console.log('@@@',this.analytics)
     });
   }
 }
