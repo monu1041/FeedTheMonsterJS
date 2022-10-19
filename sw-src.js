38b7d6f408c836b95a084cd1b453f963ce9798d5
ashwaraj <35664866+ashishkrr@users.noreply.github.com>
2022-10-19 14:40:01 +0530

    cache audio files

--- sw-src.js
+++ sw-src.js
@@ -2,7 +2,6 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
 );
 
-let messageEvent;
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 console.log(self);
@@ -12,8 +11,11 @@ self.addEventListener('activate', function(e) {
     // console.log(e);
 });
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', async function(e) {
     console.log("install");
+    let cacheName = await getCacheName();
+    console.log(cacheName);
+
     self.skipWaiting();
     // console.log(e);
 });
@@ -23,7 +25,6 @@ self.registration.addEventListener('updatefound', function(e) {
     caches.keys().then(cacheNames => {
         cacheNames.forEach(cacheName => {
             caches.delete(cacheName);
-            console.log("ghgh");
             self.clients.matchAll().then(clients => {
                 clients.forEach(client => client.postMessage({msg: 'Update Found'}));
             })
@@ -34,3 +35,34 @@ self.registration.addEventListener('updatefound', function(e) {
 
 
 
+function cacheAudiosFiles(file, cacheName) {
+    caches.open(cacheName).then(function (cache) {
+      console.log("sw: adding AUdio files");
+      return cache.add(file);
+    });
+  }
+
+function getCacheName() {
+    caches.keys().then(cacheNames => {
+        cacheNames.forEach(cacheName => {
+            getALLAudioUrls(cacheName)
+        })
+    });
+}
+
+function getALLAudioUrls(cacheName) { 
+    fetch("./ftm_english.json", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    }).then((res) =>
+    res.json().then((data) => {
+      for (let i = 0; i < 10; i++) {
+        data.Levels[i].Puzzles.forEach((element) => {
+            cacheAudiosFiles(element.prompt.PromptAudio, cacheName);
+        });
+      }
+    })
+  );
+}
