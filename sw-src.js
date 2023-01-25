5990588f80eac2fe2afe72ad1e4dadf3e4d239ad
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2023-01-25 11:43:50 +0530

    Revert "AdditionalChanges"
    This reverts commit 55a34f6c01a1f8f9362fe4f2d7b3fb2f783ef350.

--- sw-src.js
+++ sw-src.js
@@ -1,58 +1,62 @@
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+    'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
 );
 
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
 
 // self.addEventListener('activate', function(e) {
 //     console.log("activated");
 //     // console.log(e);
 // });
 
-self.addEventListener("install", async function (e) {
-  let cacheName = await getCacheName();
-  self.skipWaiting();
+self.addEventListener('install', async function(e) {
+    let cacheName = await getCacheName();
+    self.skipWaiting();
 });
 
-self.registration.addEventListener("updatefound", function (e) {
-  caches.keys().then((cacheNames) => {
-    cacheNames.forEach((cacheName) => {
-      caches.delete(cacheName);
-      self.clients.matchAll().then((clients) => {
-        clients.forEach((client) =>
-          client.postMessage({ msg: "Update Found" })
-        );
-      });
+self.registration.addEventListener('updatefound', function(e) {
+  
+    caches.keys().then(cacheNames => {
+        cacheNames.forEach(cacheName => {
+            caches.delete(cacheName);
+            self.clients.matchAll().then(clients => {
+                clients.forEach(client => client.postMessage({msg: 'Update Found'}));
+            })
+        });
     });
-  });
 });
+
+
+
 
 function cacheAudiosFiles(file, cacheName) {
-  caches.open(cacheName).then(function (cache) {
-    return cache.add(file);
-  });
-}
+    caches.open(cacheName).then(function (cache) {
+      return cache.add(file);
+    });
+  }
 
 function getCacheName() {
-  caches.keys().then((cacheNames) => {
-    cacheNames.forEach((cacheName) => {
-      getALLAudioUrls(cacheName);
+    caches.keys().then(cacheNames => {
+        cacheNames.forEach(cacheName => {
+            getALLAudioUrls(cacheName)
+        })
     });
-  });
 }
 
-function getALLAudioUrls(cacheName) {
-  let lang = "english";
-  fetch("./lang/" + lang + "/ftm_" + lang + ".json", {
+function getALLAudioUrls(cacheName) { 
+    let lang = "english";
+    fetch("./lang/"+lang+"/ftm_"+lang+".json", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((res) =>
+    }).then((res) =>
     res.json().then((data) => {
-      for (let i = 0; i < 150; i++) {
+      for (let i = 0; i < 10; i++) {
         data.Levels[i].Puzzles.forEach((element) => {
-          cacheAudiosFiles(element.prompt.PromptAudio, cacheName);
+            cacheAudiosFiles(element.prompt.PromptAudio, cacheName);
         });
       }
     })
